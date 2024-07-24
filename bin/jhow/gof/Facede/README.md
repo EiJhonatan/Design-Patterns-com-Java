adf0fa7bac7035d55e75c841791122f7a6f0f1ca
jhonatan ferreira <devjhow2003@gmail.com>
2024-07-24 00:29:58 -0300

    adcionando facade

---
+++ bin/jhow/gof/Facede/README.md
@@ -0,0 +1,7 @@
+# praticando com java FACADE.
+
+
+![alt text](imgs/image.png)
+<br>
+
+![alt text](imgs/image-1.png)
---
+++ src/jhow/gof/Facede/test.java
@@ -0,0 +1,11 @@
+package jhow.gof.Facede;
+
+public class test {
+   
+   public static void main(String[] args) {
+        //facade
+        Facade facade = new Facade();
+        facade.migrarCliente("jhow", "123456789");
+    }
+    
+   }
